1940ae7d9ceb910def76d32129e1aaed0564a3a6
chacruna43 <grabek30@gmail.com>
2023-11-16 21:19:13 +0000

    zadanie nr4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Ewa";
+string gender = "Kobieta";
+int age = 33;
+
+if (gender == "Kobieta" && name == "Ewa" && age > 36)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age < 16)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa ma 33 lat");
+}
+else if (name == "Ewa" && age == 33) 
+{
+    Console.WriteLine("Siedziałem nad tym zadaniem dwa dni ! Mam nadzieje że jest poprawnie zrobione");
+}
