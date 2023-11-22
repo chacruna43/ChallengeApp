616969a920041d49cd1eb3efc1ff66546418f280
chacruna43 <grabek30@gmail.com>
2023-11-22 14:35:42 +0000

    zadanie nr 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -7,15 +7,15 @@ string gender = "female";
 int age = 17;
 
 
-if(gender == "female" && age < 30)
-{ 
+if (gender == "female" && age < 30)
+{
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
- else if(name == "Ewa" && age == 30)
-{ 
+else if (name == "Ewa" && age == 33)
+{
     Console.WriteLine("EWa lat 33");
 }
- else if(gender != "female" && age < 18)
+else if (gender != "female" && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
